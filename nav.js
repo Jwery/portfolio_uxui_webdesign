45a7e9b791d341fe684cc683de3f0c872e89a571
JoatypeR <weryjoachim@gmail.com>
2022-10-04 14:38:53 +0100

    Implémentation nav

--- nav.js
+++ nav.js
@@ -1,21 +1,33 @@
-var lastScrollTop; // This Varibale will store the top position
-
-navbar = document.getElementById('navbar'); // Get The NavBar
-
-window.addEventListener('scroll',function(){
- //on every scroll this funtion will be called
-  
-  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  //This line will get the location on scroll
-  
-  if(scrollTop > lastScrollTop){ //if it will be greater than the previous
-    navbar.style.top='-80px';
-    //set the value to the negetive of height of navbar 
-  }
-  
-  else{
-    navbar.style.top='0';
-  }
-  
-  lastScrollTop = scrollTop; //New Position Stored
+document.querySelector('header button').addEventListener('click', function() {
+  document.querySelector('header nav').classList.toggle('open');
+  document.querySelector('header button').classList.toggle('is-active');
+});
+// scrollY
+window.addEventListener('scroll', function() {
+    console.log(window.scrollY);
+    if(window.scrollY > 100) {
+        document.querySelector('footer button').style.display = 'block';
+    } else {
+        document.querySelector('footer button').style.display = 'none';
+    }
+});
+// scroll to top
+document.querySelector('footer button').addEventListener('click', function() {
+    window.scrollTo(
+        {
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        }
+    );
+});
+// Mouse coordinates
+window.addEventListener('mousemove', function(event) {
+    //console.log(event.clientX, event.clientY);
+    document.querySelector('#mouseCoordinates').innerHTML = event.clientX + ' ' + event.clientY;
+});
+// keyboard keydown
+window.addEventListener('keydown', function(event) {
+    //console.log(event.key);
+    document.querySelector('#keyboard').innerHTML = event.key;
 });
